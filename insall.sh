b35e86fae3e96d06e13f68f360b897bd6392e6bc
Tushryadav <ty780003@gmail.com>
2026-05-13 11:50:33 +0530

    Refactor installation script for clarity and updates
    Removed commented-out steps and adjusted comments for clarity. Added commands for copying gcloud and kubeconfig to Jenkins home.

--- insall.sh
+++ insall.sh
@@ -73,9 +73,7 @@ sudo systemctl enable docker
 sudo usermod -aG docker jenkins
 sudo chmod 666 /var/run/docker.sock
 
-# FIX: `newgrp docker` spawns an interactive subshell and HALTS the script.
-
-Step 5: Install Jenkins
+# Step 5: Install Jenkins
 sudo apt install -y fontconfig openjdk-21-jre
 java -version
 
@@ -94,7 +92,6 @@ sudo systemctl enable jenkins
 sudo systemctl start jenkins
 
 # Print initial admin password
-# "Jenkins initial admin password:"
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
 
 # Step 6: Enable Filestore CSI Driver on GKE
@@ -120,17 +117,121 @@ kubectl -n ingress-nginx rollout status deploy/ingress-nginx-controller
 # Get the external IP
 kubectl -n ingress-nginx get svc ingress-nginx-controller
 
-# Step 8: Verify Metrics Server"
-#kubectl -n kube-system get deploy metrics-server
-#kubectl top nodes
+# gcloud access — copy gcloud config to jenkins home
+sudo mkdir -p /var/lib/jenkins/.config/gcloud
+sudo cp -r ~/.config/gcloud/* /var/lib/jenkins/.config/gcloud/
+sudo chown -R jenkins:jenkins /var/lib/jenkins/.config
 
-# Step 9: Detect Public IP & Patch Values"
+# kubectl access — copy kubeconfig to jenkins home
+sudo mkdir -p /var/lib/jenkins/.kube
+sudo cp ~/.kube/config /var/lib/jenkins/.kube/config
+sudo chown -R jenkins:jenkins /var/lib/jenkins/.kube
 
-PUBLIC_IP=$(curl -s ifconfig.me)
-echo "Your public IP: $PUBLIC_IP"
+# Step 10: Verify Values Files"
 
-sed -i "s|<public_ip>|$PUBLIC_IP|g" helm/values/values-rocketchat.yaml
-sed -i "s|http://rocketchat.local|http://$PUBLIC_IP.nip.io|g" helm/values/values-rocketchat.yaml
+grep -E "host|rootUrl|rootPassword|storageClassName" \
+  helm/values/values-rocketchat.yaml \
+  helm/values/values-db.yaml
+
+kubectl create namespace rocketchat
+kubectl create namespace rocketchat-db
+kubectl create namespace rocketchat-nginx
+
+
+
+gcloud container clusters get-credentials staging --region asia-south2 --project project-d3f73645-327e-4f11-ba2
+kubectl get nodes
+
+#  gcloud config
+gcloud auth login
+gcloud config set account 440563071013-compute@developer.gserviceaccount.com
+gcloud auth list
+#  Verify your GCP project ID — the one below may be incomplete/incorrect.
+gcloud config set project project-d3f73645-327e-4f11-ba2
+gcloud config get-value project
+
+# Step 3: Install Helm"
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+# Verify
+helm version
+
+#Step 4: Install Docker
+sudo apt remove --ignore-missing -y \
+  docker.io docker-compose docker-compose-v2 docker-doc \
+  podman-docker containerd runc || true
+
+# Add Docker's official GPG key
+sudo apt update
+sudo apt install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
+  -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+sudo apt install -y \
+  docker-ce docker-ce-cli containerd.io \
+  docker-buildx-plugin docker-compose-plugin
+
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker jenkins
+sudo chmod 666 /var/run/docker.sock
+
+# Step 5: Install Jenkins
+sudo apt install -y fontconfig openjdk-21-jre
+java -version
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ \
+  | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt update
+sudo apt install -y jenkins
+
+# Enable & start Jenkins
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+# "Jenkins initial admin password:"
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+# Step 6: Enable Filestore CSI Driver on GKE
+gcloud container clusters update staging \
+  --update-addons=GcpFilestoreCsiDriver=ENABLED \
+  --zone=asia-south2
+
+# verify storageclass
+kubectl get storageclass
+
+# Step 7: Install Nginx Ingress Controller"
+
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+
+helm install ingress-nginx ingress-nginx/ingress-nginx \
+  --namespace ingress-nginx \
+  --create-namespace
+
+# Wait for it to come up
+kubectl -n ingress-nginx rollout status deploy/ingress-nginx-controller
+
+# Get the external IP
+kubectl -n ingress-nginx get svc ingress-nginx-controller
 
 # gcloud access — copy gcloud config to jenkins home
 sudo mkdir -p /var/lib/jenkins/.config/gcloud
@@ -152,13 +253,119 @@ kubectl create namespace rocketchat
 kubectl create namespace rocketchat-db
 kubectl create namespace rocketchat-nginx
 
-# Create k8s image pull secret
-#kubectl create secret docker-registry gar-secret \
-#  --docker-server=asia-south2-docker.pkg.dev \
-#  --docker-username=oauth2accesstoken \
-#  --docker-password="$(gcloud auth print-access-token)" \
-#  --docker-email=440563071013-compute@developer.gserviceaccount.com \
-#  --namespace rocketchat
+gcloud container clusters get-credentials dev --region asia-south2 --project project-d3f73645-327e-4f11-ba2
+kubectl get nodes
+
+#  gcloud config
+gcloud auth login
+gcloud config set account 440563071013-compute@developer.gserviceaccount.com
+gcloud auth list
+#  Verify your GCP project ID — the one below may be incomplete/incorrect.
+gcloud config set project project-d3f73645-327e-4f11-ba2
+gcloud config get-value project
+
+# Step 3: Install Helm"
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+# Verify
+helm version
+
+#Step 4: Install Docker
+sudo apt remove --ignore-missing -y \
+  docker.io docker-compose docker-compose-v2 docker-doc \
+  podman-docker containerd runc || true
+
+# Add Docker's official GPG key
+sudo apt update
+sudo apt install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
+  -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+sudo apt install -y \
+  docker-ce docker-ce-cli containerd.io \
+  docker-buildx-plugin docker-compose-plugin
+
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker jenkins
+sudo chmod 666 /var/run/docker.sock                
+
+# Step 5: Install Jenkins
+sudo apt install -y fontconfig openjdk-21-jre
+java -version
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ \
+  | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt update
+sudo apt install -y jenkins
+
+# Enable & start Jenkins
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+# "Jenkins initial admin password:"
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+# Step 6: Enable Filestore CSI Driver on GKE
+gcloud container clusters update dev \
+  --update-addons=GcpFilestoreCsiDriver=ENABLED \
+  --zone=asia-south2
+
+# verify storageclass
+kubectl get storageclass
+
+# Step 7: Install Nginx Ingress Controller"
+
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+
+helm install ingress-nginx ingress-nginx/ingress-nginx \
+  --namespace ingress-nginx \
+  --create-namespace
+
+# Wait for it to come up
+kubectl -n ingress-nginx rollout status deploy/ingress-nginx-controller
+
+# Get the external IP
+kubectl -n ingress-nginx get svc ingress-nginx-controller
+
+# gcloud access — copy gcloud config to jenkins home
+sudo mkdir -p /var/lib/jenkins/.config/gcloud
+sudo cp -r ~/.config/gcloud/* /var/lib/jenkins/.config/gcloud/
+sudo chown -R jenkins:jenkins /var/lib/jenkins/.config
+
+# kubectl access — copy kubeconfig to jenkins home
+sudo mkdir -p /var/lib/jenkins/.kube
+sudo cp ~/.kube/config /var/lib/jenkins/.kube/config
+sudo chown -R jenkins:jenkins /var/lib/jenkins/.kube
+
+# Step 10: Verify Values Files"
+
+grep -E "host|rootUrl|rootPassword|storageClassName" \
+  helm/values/values-rocketchat.yaml \
+  helm/values/values-db.yaml
+
+kubectl create namespace rocketchat
+kubectl create namespace rocketchat-db
+kubectl create namespace rocketchat-nginx
 
 # Step 12: Pre-flight Checks"
 echo "[1] Nodes:"
